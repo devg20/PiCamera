e3fbd37c21506e426d0a18779cf4f3666fc73e81
devg20 <57871429+devg20@users.noreply.github.com>
2019-11-18 10:48:57 +0530

    Update capture.py

--- capture.py
+++ capture.py
@@ -1,15 +1,15 @@
--Open Thonny IDE in RaspberryPi and run the following code to capture a photo using the RaspberryPI Camera Module
+#Open Thonny IDE in RaspberryPi and run the following code to capture a photo using the RaspberryPI Camera Module
 
-from picamera import PiCamera //Importing required libraries
+from picamera import PiCamera #Importing required libraries
 from time import sleep
 
-camera=PiCamera()             //Creating a varible to use Picamera functions
+camera=PiCamera()             #Creating a varible to use Picamera functions
 print("Preview Started.")   
-camera.start_preview()        //Inorder to give the camera some time to focus
-camera.sleep(5)               //5 seconds preview time
+camera.start_preview()        #Inorder to give the camera some time to focus
+camera.sleep(5)               #5 seconds preview time
 camera.stop_preview()
 print("Capturing Shot")
-camera.capture("photo.jpg")   //Capturing picture
+camera.capture("photo.jpg")   #Capturing picture
 print("Picture Captured")
 
 
